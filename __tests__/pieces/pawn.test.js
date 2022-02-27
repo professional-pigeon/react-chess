baef596f7f39e13f9b07437d49fa76d8fde98d73
professional-pigeon <kyle.kayperez@gmail.com>
2022-02-26 19:50:27 -0800

    create test for returning false if pawn cannot move into a blocked square one space ahead

--- __tests__/pieces/pawn.test.js
+++ __tests__/pieces/pawn.test.js
@@ -3,29 +3,32 @@ import { pawnMovesWhite } from "../../src/helper/pawn";
 describe('pawnMoves', () => {
   const tileEmpty = {
     piece: "empty",
-    position: [1, 0]
+    position: [3, 0]
   }
 
   const tileEmptyTwo = {
     piece: "empty",
-    position: [2, 0]
+    position: [4, 0]
   }
 
   const tileBlocked = {
     piece: "piece",
-    position: [1, 0]
+    position: [3, 0]
   }
 
   const canTakeTile = {
     piece: "piece",
-    position: [1, 1]
+    position: [3, 1]
   }
 
   test('pawn can move one space forward if position is empty', () => {
-    expect(pawnMovesWhite([0, 0], tileEmpty)).toEqual(true)
+    expect(pawnMovesWhite([2, 0], tileEmpty)).toEqual(true)
+  })
+  test('pawn will return false if it cannot move one space ahead', () => {
+    expect(pawnMovesWhite([2, 0], tileEmpty)).toEqual(false)
   })
   test('pawn can move two spaces forward if position is empty and on starting position', () => {
-    expect(pawnMovesWhite([2, 0], tileEmptyTwo)).toEqual(true)
+    expect(pawnMovesWhite([2, 1], tileEmptyTwo)).toEqual(true)
   })
 
 })

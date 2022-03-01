b9653d4ee60ec5ecb4baf9a8df36fe882a704f6f
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-01 13:32:10 -0800

    import orthogonal functions from rook into queen helper

--- src/helper/Queen.js
+++ src/helper/Queen.js
@@ -79,6 +79,83 @@ function minusPlus(pos, color, board) {
   return moves
 }
 
+function right(pos, color, board) {
+  let coordinates = pos.split("")
+  let moves = []
+  for (let i = Number(coordinates[1]) + 1; i <= 8; i++) {
+    let piece = board[coordinates[0].toString() + i.toString()]
+    if (piece === "empty") {
+      moves.push(coordinates[0].toString() + i.toString())
+    }
+    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
+      moves.push(coordinates[0].toString() + i.toString())
+      break
+    }
+    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
+      break
+    }
+  }
+  return moves
+}
+
+function left(pos, color, board) {
+  let coordinates = pos.split("")
+  let moves = []
+  for (let i = Number(coordinates[1]) - 1; i >= 1; i--) {
+    let piece = board[coordinates[0].toString() + i.toString()]
+    if (piece === "empty") {
+      moves.push(coordinates[0].toString() + i.toString())
+    }
+    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
+      moves.push(coordinates[0].toString() + i.toString())
+      break
+    }
+    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
+      break
+    }
+  }
+  return moves
+
+}
+
+function down(pos, color, board) {
+  let coordinates = pos.split("")
+  let moves = []
+  for (let i = Number(coordinates[0]) - 1; i >= 1; i--) {
+    let piece = board[i.toString() + coordinates[1].toString()]
+    if (piece === "empty") {
+      moves.push(i.toString() + coordinates[1].toString())
+    }
+    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
+      moves.push(i.toString() + coordinates[1].toString())
+      break
+    }
+    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
+      break
+    }
+  }
+  return moves
+}
+
+function up(pos, color, board) {
+  let coordinates = pos.split("")
+  let moves = []
+  for (let i = Number(coordinates[0]) + 1; i <= 8; i++) {
+    let piece = board[i.toString() + coordinates[1].toString()]
+    if (piece === "empty") {
+      moves.push(i.toString() + coordinates[1].toString())
+    }
+    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
+      moves.push(i.toString() + coordinates[1].toString())
+      break
+    }
+    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
+      break
+    }
+  }
+  return moves
+}
+
 function queenMoves(pos, color, board) {
 
 }

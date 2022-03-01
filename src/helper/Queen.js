44ecbb800d7df1d475263f687f009daa53f4cf98
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-01 13:31:04 -0800

    create queen helper functions, import diagonals logic from bishops and rename for queen functions

---
+++ src/helper/Queen.js
@@ -0,0 +1,86 @@
+function plusPlus(pos, color, board) {
+  let coordinates = pos.split("")
+  let x = Number(coordinates[0])
+  let y = Number(coordinates[1])
+  let moves = []
+  let high = x >= y ? x : y
+  for (let i = 1; high < 8; i++) {
+    let tile = (x + i).toString() + (y + i).toString()
+    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
+      break
+    }
+    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
+      moves.push(tile)
+      break
+    }
+    moves.push(tile)
+    high++ 
+  }
+  return moves
+}
+
+function minusMinus(pos, color, board) {
+  let coordinates = pos.split("")
+  let x = Number(coordinates[0])
+  let y = Number(coordinates[1])
+  let moves = []
+  let low = x <= y ? x : y
+  for (let i = 1; i < low; i++) {
+    let tile = (x - i).toString() + (y - i).toString()
+    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
+      break
+    }
+    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
+      moves.push(tile)
+      break
+    }
+    moves.push(tile)
+  }
+  return moves
+}
+
+function plusMinus(pos, color, board) {
+  let coordinates = pos.split("")
+  let x = Number(coordinates[0])
+  let y = Number(coordinates[1])
+  let moves = []
+  let loopN = (8 - x) <= (y -1) ? (8 - x) : (y -1)
+  for(let i = 1; i <= loopN; i++) {
+    let tile = (x + i).toString() + (y - i).toString()
+    if (board[tile].includes(color)) {
+      break
+    }
+    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
+      moves.push(tile)
+      break
+    }
+    moves.push(tile)
+  }
+  return moves
+}
+
+function minusPlus(pos, color, board) {
+  let coordinates = pos.split("")
+  let x = Number(coordinates[0])
+  let y = Number(coordinates[1])
+  let moves = []
+  let loopN = (8 - y) >= (x - 1) ? (x - 1) : (8 - y)
+  for (let i = 1; i <= loopN; i++) {
+    let tile = (x - i).toString() + (y + i).toString()
+    if (board[tile].includes(color)) {
+      break
+    }
+    if (board[tile].includes("Black") && color === "White" || board[tile].includes("White") && color === "Black") {
+      moves.push(tile)
+      break
+    }
+    moves.push(tile)
+  }
+  return moves
+}
+
+function queenMoves(pos, color, board) {
+
+}
+
+export { queenMoves }

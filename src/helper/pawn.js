14f6a7d0ccf3b78aa254c2e4bd43776e25875776
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-01 13:42:57 -0800

    Recreate pawn moves naming four functoins pawns will need: pawnForward, pawnTakes, enPassant, switchPiece

--- src/helper/pawn.js
+++ src/helper/pawn.js
@@ -1,48 +1,19 @@
-function pawnMovesWhite(pos, tile) {
-  let bool = false
-  let shiftRow = tile.position[0] - pos[0]
-  let shiftCol = tile.position[1] - pos[1]
-  if (tile.piece === "empty") {
-    if (shiftRow === 1 && shiftCol === 0) {
-      bool = true
-    }
-    if (shiftRow === 2) {
-      bool = true
-    }
-  }
-  if (shiftRow === 2 && pos[0] === 2 && tile.piece === "empty") {
-    bool = true
-  }
-  if (shiftRow === 1 && tile.piece != "empty" && shiftLeft === 1) {
-    bool = true
-  }
-  return bool
+function PawnForward(pos, color, board) {
+  let moves = []
+  return moves
 }
 
-const tileEmpty = {
-  piece: "empty",
-  position: [1, 0]
+function pawnTakes(pos, color, board) {
+  let moves = []
+  return moves
 }
 
-const tileEmptyTwo = {
-  piece: "empty",
-  position: [2, 0]
+function enPassant(pos, color, board) {
+  let moves = []
+  return moves
 }
 
-const tileBlocked = {
-  piece: "piece",
-  position: [1, 0]
+function switchPiece(pos, color, board) {
+  let thing = ""
+  return thing
 }
-
-const canTakeTile = {
-  piece: "piece",
-  position: [1, 1]
-}
-
-function pawnOne(pos) {
-  pos[0]++
-  return pos
-}
-
-
-console.log(pawnOne([0,0]))

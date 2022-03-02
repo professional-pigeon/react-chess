cff20d00ff26d6fd42d62dd9fe47871d0e871cb5
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-02 13:49:05 -0800

    Code cleanup: Queen helper functions wrap conditions in if statements to clarify order of operations

--- src/helper/Knight.js
+++ src/helper/Knight.js
@@ -1,18 +1,18 @@
 function knightMoves(pos, color, board) {
-  let coordinates = pos.split('')
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let arr = []
-  let posArr = [[2, -1], [2, 1], [-2, -1], [-2, 1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+  let coordinates = pos.split('');
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let arr = [];
+  let posArr = [[2, -1], [2, 1], [-2, -1], [-2, 1], [1, 2], [-1, 2], [1, -2], [-1, -2]];
   posArr.forEach(function(coord) {
-    let tile = (x + coord[0]).toString() + (y + coord[1]).toString()
+    let tile = (x + coord[0]).toString() + (y + coord[1]).toString();
     if (board[tile] !== undefined) {
       if (board[tile].includes(color) === false) {
-        arr.push(tile)
+        arr.push(tile);
       }
     }
   })
-  return arr
+  return arr;
 }
 
 export default knightMoves
--- src/helper/Queen.js
+++ src/helper/Queen.js
@@ -1,172 +1,172 @@
 function plusPlus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let high = x >= y ? x : y
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let high = x >= y ? x : y;
   for (let i = 1; high < 8; i++) {
-    let tile = (x + i).toString() + (y + i).toString()
-    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
-      break
+    let tile = (x + i).toString() + (y + i).toString();
+    if ((board[tile].includes("White") && color === "White") || (board[tile].includes("Black") && color === "Black")) {
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black" )|| (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
-    high++ 
+    moves.push(tile);
+    high++;
   }
-  return moves
+  return moves;
 }
 
 function minusMinus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let low = x <= y ? x : y
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let low = x <= y ? x : y;
   for (let i = 1; i < low; i++) {
-    let tile = (x - i).toString() + (y - i).toString()
-    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
-      break
+    let tile = (x - i).toString() + (y - i).toString();
+    if ((board[tile].includes("White") && color === "White") || (board[tile].includes("Black") && color === "Black")) {
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black") || (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
   return moves
 }
 
 function plusMinus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let loopN = (8 - x) <= (y -1) ? (8 - x) : (y -1)
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let loopN = (8 - x) <= (y -1) ? (8 - x) : (y -1);
   for(let i = 1; i <= loopN; i++) {
-    let tile = (x + i).toString() + (y - i).toString()
+    let tile = (x + i).toString() + (y - i).toString();
     if (board[tile].includes(color)) {
-      break
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black") || (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
   return moves
 }
 
 function minusPlus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let loopN = (8 - y) >= (x - 1) ? (x - 1) : (8 - y)
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let loopN = (8 - y) >= (x - 1) ? (x - 1) : (8 - y);
   for (let i = 1; i <= loopN; i++) {
-    let tile = (x - i).toString() + (y + i).toString()
+    let tile = (x - i).toString() + (y + i).toString();
     if (board[tile].includes(color)) {
-      break
+      break;
     }
-    if (board[tile].includes("Black") && color === "White" || board[tile].includes("White") && color === "Black") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("Black") && color === "White") || (board[tile].includes("White") && color === "Black")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
   return moves
 }
 
 function right(pos, color, board) {
-  let coordinates = pos.split("")
-  let moves = []
+  let coordinates = pos.split("");
+  let moves = [];
   for (let i = Number(coordinates[1]) + 1; i <= 8; i++) {
-    let piece = board[coordinates[0].toString() + i.toString()]
+    let piece = board[coordinates[0].toString() + i.toString()];
     if (piece === "empty") {
-      moves.push(coordinates[0].toString() + i.toString())
+      moves.push(coordinates[0].toString() + i.toString());
     }
-    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
-      moves.push(coordinates[0].toString() + i.toString())
-      break
+    if ((piece.includes("Black") && color === "White") || (piece.includes("White") && color === "Black")) {
+      moves.push(coordinates[0].toString() + i.toString());
+      break;
     }
-    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
-      break
+    if ((piece.includes("White") && color === "White") || (piece.includes("Black") && color === "Black")) {
+      break;
     }
   }
   return moves
 }
 
 function left(pos, color, board) {
-  let coordinates = pos.split("")
-  let moves = []
+  let coordinates = pos.split("");
+  let moves = [];
   for (let i = Number(coordinates[1]) - 1; i >= 1; i--) {
-    let piece = board[coordinates[0].toString() + i.toString()]
+    let piece = board[coordinates[0].toString() + i.toString()];
     if (piece === "empty") {
-      moves.push(coordinates[0].toString() + i.toString())
+      moves.push(coordinates[0].toString() + i.toString());
     }
-    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
-      moves.push(coordinates[0].toString() + i.toString())
-      break
+    if ((piece.includes("Black") && color === "White") || (piece.includes("White") && color === "Black")) {
+      moves.push(coordinates[0].toString() + i.toString());
+      break;
     }
-    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
-      break
+    if ((piece.includes("White") && color === "White") || (piece.includes("Black") && color === "Black")) {
+      break;
     }
   }
-  return moves
+  return moves;
 
 }
 
 function down(pos, color, board) {
-  let coordinates = pos.split("")
-  let moves = []
+  let coordinates = pos.split("");
+  let moves = [];
   for (let i = Number(coordinates[0]) - 1; i >= 1; i--) {
-    let piece = board[i.toString() + coordinates[1].toString()]
+    let piece = board[i.toString() + coordinates[1].toString()];
     if (piece === "empty") {
-      moves.push(i.toString() + coordinates[1].toString())
+      moves.push(i.toString() + coordinates[1].toString());
     }
-    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
-      moves.push(i.toString() + coordinates[1].toString())
-      break
+    if ((piece.includes("Black") && color === "White") || (piece.includes("White") && color === "Black")) {
+      moves.push(i.toString() + coordinates[1].toString());
+      break;
     }
-    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
-      break
+    if ((piece.includes("White") && color === "White") || (piece.includes("Black") && color === "Black")) {
+      break;
     }
   }
-  return moves
+  return moves;
 }
 
 function up(pos, color, board) {
-  let coordinates = pos.split("")
-  let moves = []
+  let coordinates = pos.split("");
+  let moves = [];
   for (let i = Number(coordinates[0]) + 1; i <= 8; i++) {
-    let piece = board[i.toString() + coordinates[1].toString()]
+    let piece = board[i.toString() + coordinates[1].toString()];
     if (piece === "empty") {
-      moves.push(i.toString() + coordinates[1].toString())
+      moves.push(i.toString() + coordinates[1].toString());
     }
-    if (piece.includes("Black") && color === "White" || piece.includes("White") && color === "Black") {
-      moves.push(i.toString() + coordinates[1].toString())
-      break
+    if ((piece.includes("Black") && color === "White") || (piece.includes("White") && color === "Black")) {
+      moves.push(i.toString() + coordinates[1].toString());
+      break;
     }
-    if (piece.includes("White") && color === "White" || piece.includes("Black") && color === "Black") {
-      break
+    if ((piece.includes("White") && color === "White") || (piece.includes("Black") && color === "Black")) {
+      break;
     }
   }
-  return moves
+  return moves;
 }
 
 function queenMoves(pos, color, board) {
-  let arr = []
-  arr.push(up(pos, color, board))
-  arr.push(down(pos, color, board))
-  arr.push(right(pos, color, board))
-  arr.push(left(pos, color, board))
-  arr.push(plusPlus(pos, color, board))
-  arr.push(minusPlus(pos, color, board))
-  arr.push(minusMinus(pos, color, board))
-  arr.push(plusMinus(pos, color, board))
-  return arr.flat()
+  let arr = [];
+  arr.push(up(pos, color, board));
+  arr.push(down(pos, color, board));
+  arr.push(right(pos, color, board));
+  arr.push(left(pos, color, board));
+  arr.push(plusPlus(pos, color, board));
+  arr.push(minusPlus(pos, color, board));
+  arr.push(minusMinus(pos, color, board));
+  arr.push(plusMinus(pos, color, board));
+  return arr.flat();
 }
 
 export default queenMoves

6f89e065605931c3f6b77d5469380f69b50c0a91
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-02 13:43:48 -0800

    Code cleanup: in bishop helper wrap && conditions in if statements in () to avoid errors in order of operations

--- src/helper/Bishop.js
+++ src/helper/Bishop.js
@@ -66,92 +66,92 @@ const boardState = {
 }
 
 function plusPlus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let high = x >= y ? x : y
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let high = x >= y ? x : y;
   for (let i = 1; high < 8; i++) {
     let tile = (x + i).toString() + (y + i).toString()
-    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
-      break
+    if ((board[tile].includes("White") && color === "White") || (board[tile].includes("Black") && color === "Black")) {
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black") || (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
-    high++ 
+    moves.push(tile);
+    high++ ;
   }
-  return moves
+  return moves;
 }
 
 function minusMinus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let low = x <= y ? x : y
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let low = x <= y ? x : y;
   for (let i = 1; i < low; i++) {
-    let tile = (x - i).toString() + (y - i).toString()
-    if (board[tile].includes("White") && color === "White" || board[tile].includes("Black") && color === "Black") {
-      break
+    let tile = (x - i).toString() + (y - i).toString();
+    if ((board[tile].includes("White") && color === "White") || (board[tile].includes("Black") && color === "Black")) {
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black") || (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
-  return moves
+  return moves;
 }
 
 function plusMinus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let loopN = (8 - x) <= (y -1) ? (8 - x) : (y -1)
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let loopN = (8 - x) <= (y -1) ? (8 - x) : (y -1);
   for(let i = 1; i <= loopN; i++) {
-    let tile = (x + i).toString() + (y - i).toString()
+    let tile = (x + i).toString() + (y - i).toString();
     if (board[tile].includes(color)) {
-      break
+      break;
     }
-    if (board[tile].includes("White") && color === "Black" || board[tile].includes("Black") && color === "White") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("White") && color === "Black") || (board[tile].includes("Black") && color === "White")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
-  return moves
+  return moves;
 }
 
 function minusPlus(pos, color, board) {
-  let coordinates = pos.split("")
-  let x = Number(coordinates[0])
-  let y = Number(coordinates[1])
-  let moves = []
-  let loopN = (8 - y) >= (x - 1) ? (x - 1) : (8 - y)
+  let coordinates = pos.split("");
+  let x = Number(coordinates[0]);
+  let y = Number(coordinates[1]);
+  let moves = [];
+  let loopN = (8 - y) >= (x - 1) ? (x - 1) : (8 - y);
   for (let i = 1; i <= loopN; i++) {
-    let tile = (x - i).toString() + (y + i).toString()
+    let tile = (x - i).toString() + (y + i).toString();
     if (board[tile].includes(color)) {
-      break
+      break;
     }
-    if (board[tile].includes("Black") && color === "White" || board[tile].includes("White") && color === "Black") {
-      moves.push(tile)
-      break
+    if ((board[tile].includes("Black") && color === "White") || (board[tile].includes("White") && color === "Black")) {
+      moves.push(tile);
+      break;
     }
-    moves.push(tile)
+    moves.push(tile);
   }
-  return moves
+  return moves;
 }
 
 function bishopMoves(pos, color, board) {
-  let arr = []
-  arr.push(plusPlus(pos, color, board))
-  arr.push(minusPlus(pos, color, board))
-  arr.push(plusMinus(pos, color, board))
-  arr.push(minusMinus(pos, color, board))
-  return arr.flat()
+  let arr = [];
+  arr.push(plusPlus(pos, color, board));
+  arr.push(minusPlus(pos, color, board));
+  arr.push(plusMinus(pos, color, board));
+  arr.push(minusMinus(pos, color, board));
+  return arr.flat();
 }
 export { bishopMoves, plusPlus, minusMinus, plusMinus, minusPlus }

7c32c68c435dd79915fc106b6ab78b036a4525f6
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-17 09:39:32 -0700

    add board state to database through board

--- src/components/Board.js
+++ src/components/Board.js
@@ -17,6 +17,80 @@ function Board() {
     console.log(data.docs)
   }
 
+
+  async function setNew(id) {
+    const docRef = db.collection('boardStates').doc('initial')
+
+    await docRef.set({
+      11: "Rook White",
+      12: "Knight White",
+      13: "Bishop White",
+      14: "Queen White",
+      15: "King White",
+      16: "Bishop White",
+      17: "Knight White",
+      18: "Rook White",
+      21: "Pawn White",
+      22: "Pawn White",
+      23: "Pawn White",
+      24: "Pawn White",
+      25: "Pawn White",
+      26: "Pawn White",
+      27: "Pawn White",
+      28: "Pawn White",
+      31: "empty",
+      32: "empty",
+      33: "empty",
+      34: "empty",
+      35: "empty",
+      36: "empty",
+      37: "empty",
+      38: "empty",
+      41: "empty",
+      42: "empty",
+      43: "empty",
+      44: "empty",
+      45: "empty",
+      46: "empty",
+      47: "empty",
+      48: "empty",
+      51: "empty",
+      52: "empty",
+      53: "empty",
+      54: "empty",
+      55: "empty",
+      56: "empty",
+      57: "empty",
+      58: "empty",
+      61: "empty",
+      62: "empty",
+      63: "empty",
+      64: "empty",
+      65: "empty",
+      66: "empty",
+      67: "empty",
+      68: "empty",
+      71: "Pawn Black",
+      72: "Pawn Black",
+      73: "Pawn Black",
+      74: "Pawn Black",
+      75: "Pawn Black",
+      76: "Pawn Black",
+      77: "Pawn Black",
+      78: "Pawn Black",
+      81: "Rook Black",
+      82: "Knight Black",
+      83: "Bishop Black",
+      84: "King Black",
+      85: "Queen Black",
+      86: "Bishop Black",
+      87: "Knight Black",
+      88: "Rook Black",
+      moveHistory: [],
+      id: id
+    })
+  }
+
   let boardState = {
     11: "Rook White",
     12: "Knight White",
@@ -90,11 +164,11 @@ function Board() {
   const [chosenPiece, setPiece] = useState("empty");
   let grid = [];
 
-  function moveTake(position, chosenPiece, boardState) {
-    let movePiece = boardState[chosenPiece]
-    boardState[position] = movePiece
-    boardState[chosenPiece] = "empty"
-  }
+  // function moveTake(position, chosenPiece, boardState) {
+  //   let movePiece = boardState[chosenPiece]
+  //   boardState[position] = movePiece
+  //   boardState[chosenPiece] = "empty"
+  // }
 
   for (let i = 1; i < 9; i ++) {
     grid.push([]);

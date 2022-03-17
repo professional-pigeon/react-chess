d7b6c5ac479af282bbe9661592cb33acc23160d2
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-17 12:11:45 -0700

    add and fail first king test: kingOne should let the king move one space in any direction if it is empty

--- src/helper/King.js
+++ src/helper/King.js
@@ -1,17 +1,17 @@
 function kingOne() {
-
+  return "not working"
 }
 
 function kingCheck() {
-
+  return "not working"
 }
 
 function kingMoves() {
-
+  return "not working"
 }
 
 function kingTakes() {
-
+  return "not working"
 }
 
 export {

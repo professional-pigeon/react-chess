2dd87d5aabc82d219dd01ce8a6749acee6230ba6
professional-pigeon <kyle.kayperez@gmail.com>
2022-03-22 12:23:35 -0700

    create castleState with some previous moves to test castle functions

--- __tests__/pieces/king.test.js
+++ __tests__/pieces/king.test.js
@@ -594,7 +594,74 @@ describe('kingCheckKing', () => {
 })
 
 describe('canKingCastle', () => {
-  test('canKingCastle will return true if king is on its starting location', () => {
-    
-  })
-})
+  let castleBoard = {
+    11: "Rook White",
+    12: "empty",
+    13: "empty",
+    14: "empty",
+    15: "King White",
+    16: "empty",
+    17: "empty",
+    18: "King White",
+    21: "empty",
+    22: "empty",
+    23: "empty",
+    24: "empty",
+    25: "empty",
+    26: "empty",
+    27: "empty",
+    28: "empty",
+    31: "empty",
+    32: "empty",
+    33: "empty",
+    34: "empty",
+    35: "empty",
+    36: "empty",
+    37: "empty",
+    38: "empty",
+    41: "empty",
+    42: "empty",
+    43: "empty",
+    44: "empty",
+    45: "empty",
+    46: "empty",
+    47: "empty",
+    48: "empty",
+    51: "empty",
+    52: "empty",
+    53: "empty",
+    54: "empty",
+    55: "empty",
+    56: "empty",
+    57: "empty",
+    58: "empty",
+    61: "empty",
+    62: "empty",
+    63: "empty",
+    64: "empty",
+    65: "empty",
+    66: "empty",
+    67: "empty",
+    68: "empty",
+    71: "empty",
+    72: "empty",
+    73: "empty",
+    74: "empty",
+    75: "empty",
+    76: "empty",
+    77: "empty",
+    78: "empty",
+    81: "Rook Black",
+    82: "empty",
+    83: "empty",
+    84: "empty",
+    85: "empty",
+    86: "empty",
+    87: "empty",
+    88: "Rook Black",
+    moveHistory: [{ piece: "Rook White", move: [ "38", "18" ]}, { piece: "Rook White", move: [ "18", "38" ]}, { piece: "King Black", move: [ "83", "84" ]}, { piece: "King Black", move: [ "84", "83" ]}]
+  }
+  test('canKingCastle will return false if king is not on its starting location', () => {
+    expect(canKingCastle("15", "White", castleBoard)).toEqual(true)
+  });
+});
